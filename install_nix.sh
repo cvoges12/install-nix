597f426a48a49908768235a9ee793fd913c9c950
cvoges12 <38054771+cvoges12@users.noreply.github.com>
2019-01-28 18:08:26 -0600

    removed wireless. moved to README

--- install_nix.sh
+++ install_nix.sh
@@ -1,9 +1,3 @@
-#strace wpa_supplicant -i <WIFIINTERFACE> -c <(wpa_passphrase <SSID> <PASSPHRASE>) 2>&1 | less # to diagnose problems
-# MAKE SURE ANY HW WIRELESS SWS ARE UNBLOCKED
-systemctl restart wpa_supplicant.service
-systemctl restart dhcpcd.service
-wpa_supplicant -B -i wlp3s0 -c <(wpa_passphrase "Voges Upstairs Network" "3142229933") 2
-iwlist wlp3s0 scan
 parted /dev/sda --script mklabel gpt
 sgdisk -og /dev/sda
 sgdisk -n 1:0:1G -c 1:"EFI" -t 1:ef02 -g /dev/sda
